f992033cac77f52eff954c35ede2e5891ba7c904
YH <ks369108@gmail.com>
2024-05-16 23:22:53 +0800

    Add Log4j

--- src/main/java/com/example/springdemo/Application.java
+++ src/main/java/com/example/springdemo/Application.java
@@ -1,13 +1,22 @@
 package com.example.springdemo;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
+    private static final Logger logger = LogManager.getLogger(Application.class);
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+
+        logger.info("I AM INFO :||||||||");
+        logger.debug("I AM DEBUG ;>>>>>>>>");
+        logger.warn("I AM WARN :DDDDDDDD");
+        logger.error("I AM ERROR :(((((((");
+        logger.fatal("I AM FATAL :O");
+    }
 
 }
